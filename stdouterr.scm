01012325615be0e27575e5a7685721b187e5100b
Jesper Räftegård <jesper@huggpunkt.org>
2013-10-22 17:57:15 +0200

    reorder imports and add explanation of why we're exporting *

--- stdouterr.scm
+++ stdouterr.scm
@@ -12,8 +12,11 @@
 ;;; ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 ;;; OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
+;; As long as most of the procedures intended for export is syntax rewriting
+;; into calls for talk-is-cheap (which isn't meant to be exported), all
+;; defined syntax and procedures might just as well be exported.
 (module stdouterr *
-(import scheme chicken data-structures srfi-1 extras)
+(import scheme chicken data-structures extras srfi-1)
 
 (define print-debug-messages?
   (let ((print-them #f))
